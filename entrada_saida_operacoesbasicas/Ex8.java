041d1cd7d47893f16f756ce34788889dfd91bd8f
anoca1011 <anoca1011@gmail.com>
2026-08-11 14:55:28 -0300

    kdjglksj

---
+++ entrada_saida_operacoesbasicas/Ex8.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Ex8{
+    public static void main(String [] args){
+        Scanner scanner = new Scanner (System.in);
+
+         int[] batata = new int[8];
+          for(int i = 0; i < 8; i++){
+            System.out.println("informe um numero; ");
+            batata[i] = scanner.nextInt();
+          }
+
+          if()
+    }
+}
